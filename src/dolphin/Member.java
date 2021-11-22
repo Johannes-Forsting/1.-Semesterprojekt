efcc1960ee40e2cd77ed3ab52fb2478d7e098535
Simon <simonffsss@gmail.com>
2021-11-22 14:20:27 +0100

    Members attributes

--- src/dolphin/Member.java
+++ src/dolphin/Member.java
@@ -1,4 +1,12 @@
 package dolphin;
 
 public class Member {
+        private boolean isCompetitive;
+        private boolean isActive;
+        private int age;
+        private String name;
+        private boolean arrears; //restance
+
+
+
 }
