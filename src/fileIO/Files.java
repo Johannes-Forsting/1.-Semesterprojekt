ff91bc67465491a11438312d45d8d912370882ed
JohannesForsting98 <johannes.forsting@gmail.com>
2021-11-22 15:44:08 +0100

    first commit

---
+++ src/fileIO/Files.java
@@ -0,0 +1,44 @@
+package fileIO;
+
+import dolphin.Member;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Files {
+    public static ArrayList<Member> getMembersFromFile() {
+        ArrayList<Member> members = new ArrayList<>();
+        try {
+            File f = new File("src/resources/Members.csv");
+            Scanner scanner = new Scanner(f);
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] info = line.split(";");
+                String name =  info[0];
+                int age = Integer.parseInt(info[1]);
+                boolean isCompetative = false;
+                if (info[2].toLowerCase().equals("yes")){
+                    isCompetative = true;
+                }
+                boolean isActive = false;
+                if (info[3].toLowerCase().equals("yes")){
+                    isActive = true;
+                }
+                String date = info[4];
+                Member currentMember = new Member(name, age, isCompetative, isActive, date);
+                members.add(currentMember);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File not found");
+        }
+
+        return members;
+    }
+}
