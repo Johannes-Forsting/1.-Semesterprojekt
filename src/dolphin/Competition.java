caa504198d75b9fa2e060f36f09cfbb28e367a3a
Simon <simonffsss@gmail.com>
2021-11-22 15:37:24 +0100

    Classes update

--- src/dolphin/Competition.java
+++ src/dolphin/Competition.java
@@ -1,4 +1,28 @@
 package dolphin;
 
+import java.sql.Time;
+import java.util.ArrayList;
+
 public class Competition {
+    private Diciplin diciplin;
+    private Team team;
+    private ArrayList<Member> contestants;
+    private String place;
+    private Time time;
+    private ArrayList<String> competitionResults;
+
+    public Competition(Diciplin diciplin, Team team, ArrayList<Member> contestants, String place, Time time) {
+        this.diciplin = diciplin;
+        this.team = team;
+        this.contestants = contestants;
+        this.place = place;
+        this.time = time;
+    }
+
+    public void addContestantsToCompetition(ArrayList<Member> x){
+
+    }
+
+
+
 }
--- src/dolphin/Diciplin.java
+++ src/dolphin/Diciplin.java
@@ -1,4 +1,22 @@
 package dolphin;
 
+import java.util.ArrayList;
+
 public class Diciplin {
+    private String diciplinName;
+    private ArrayList<Member> diciplinMembers;
+
+    public Diciplin(String diciplinName, ArrayList<Member> diciplinMembers) {
+        this.diciplinName = diciplinName;
+        this.diciplinMembers = diciplinMembers;
+    }
+
+    public void addMemberToDiciplin(Member member){
+        diciplinMembers.add(member);
+    }
+
+    public ArrayList<Member> getDiciplinMembers() {
+        return diciplinMembers;
+    }
+
 }
