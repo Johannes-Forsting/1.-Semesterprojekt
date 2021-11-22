cb6f129f2b9ba6bfd218e90c206f74f45ce4f3d0
Simon <simonffsss@gmail.com>
2021-11-22 15:10:01 +0100

    Team temp

--- src/dolphin/Team.java
+++ src/dolphin/Team.java
@@ -1,4 +1,43 @@
 package dolphin;
 
+import java.util.ArrayList;
+
 public class Team {
+        private String name;
+        private ArrayList<Double> results;
+        private ArrayList<Member> contestants;
+        private ArrayList<Double> bestTimes;
+
+    public Team(String name, ArrayList<Member> contestants) {
+        this.name = name;
+        this.contestants = contestants;
+    }
+
+    public void setContestantsResults(ArrayList results, Member member) {
+        int indexOfContestantResult = member.getMemberId();
+        //some shit thats not working == member.setSwimmingResults(results.get(indexOfContestantResult));
+        //then members will have setPR, and method call from here.
+        this.results = results;
+    }
+
+    public void setResults(/*resultsfrom file, */){
+        //reads file, and add results by memberId and time accordingly
+
+    }
+
+    public ArrayList<Double> updateBestTimes() {
+        return bestTimes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getResults() {
+        return results;
+    }
+
+    public ArrayList<Member> getContestants() {
+        return contestants;
+    }
 }
