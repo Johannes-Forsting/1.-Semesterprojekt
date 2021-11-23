924ad074ede10cc48a8bfca38caed4e4e67d4952
Simon <simonffsss@gmail.com>
2021-11-23 11:09:11 +0100

    Competition class

--- src/dolphin/Competition.java
+++ src/dolphin/Competition.java
@@ -2,6 +2,7 @@ package dolphin;
 
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Competition {
     private Diciplin diciplin;
@@ -11,20 +12,36 @@ public class Competition {
     private double time;
     private ArrayList<String> competitionResults;
 
-    public Competition(Diciplin diciplin, Team team, ArrayList<Member> contestants, String place, double time) {
+    public Competition(Diciplin diciplin, Team team, String place, double time) {
         this.diciplin = diciplin;
         this.team = team;
-        this.contestants = contestants;
         this.place = place;
         this.time = time;
     }
 
 
-
-    public void addContestantsToCompetition(ArrayList<Member> x){
-
+    public void addCompetitionResults(){
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < contestants.size(); i++) {
+            String currentContestantName = contestants.get(i).getName();
+            System.out.println("Type in time for contestant: \"" + currentContestantName + "\":");
+            competitionResults.add(sc.nextLine());
+        }
+        System.out.println("Competition results: \n" + competitionResults);
     }
 
 
+    public void addContestantsToCompetition(ArrayList<Member> teamMembers){
+        for (int i = 0; i < teamMembers.size(); i++) {
+            this.contestants.add(teamMembers.get(i));
+        }
+    }
 
+    public String getPlace() {
+        return place;
+    }
+
+    public double getTime() {
+        return time;
+    }
 }
