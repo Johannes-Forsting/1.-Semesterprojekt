5b96dd169d831d8c5ba46bb913d161df10ec7017
Simon <simonffsss@gmail.com>
2021-11-26 13:16:36 +0100

    ja

--- src/fileIO/ResultObject.java
+++ src/fileIO/ResultObject.java
@@ -13,11 +13,41 @@ public class ResultObject {
         this.date = date;
         this.result = result;
     }
+    //Overload på constructor, så vi kan lave objektinstanser til metodekald uden at skulle angive parametre.
+    public ResultObject() {
+    }
 
     public ArrayList<ResultObject> crawlResultObjectCreater(){
         ArrayList<ResultObject> results = new ArrayList<>();
         for (int i = 0; i < Files.getCrawlResults().size(); i++) {
             String[] info = Files.getCrawlResults().get(i).split(";");
+            results.add( new ResultObject(info[0], info[1], info[2]));
+        }
+        return results;
+    }
+
+    public ArrayList<ResultObject> backCrawlResultObjectCreater(){
+        ArrayList<ResultObject> results = new ArrayList<>();
+        for (int i = 0; i < Files.getBackCrawlResults().size(); i++) {
+            String[] info = Files.getBackCrawlResults().get(i).split(";");
+            results.add( new ResultObject(info[0], info[1], info[2]));
+        }
+        return results;
+    }
+
+    public ArrayList<ResultObject> breastStrokeResultObjectCreater(){
+        ArrayList<ResultObject> results = new ArrayList<>();
+        for (int i = 0; i < Files.getBreastStrokeResults().size(); i++) {
+            String[] info = Files.getBreastStrokeResults().get(i).split(";");
+            results.add( new ResultObject(info[0], info[1], info[2]));
+        }
+        return results;
+    }
+
+    public ArrayList<ResultObject> butterflyResultObjectCreater(){
+        ArrayList<ResultObject> results = new ArrayList<>();
+        for (int i = 0; i < Files.getButterflyResults().size(); i++) {
+            String[] info = Files.getButterflyResults().get(i).split(";");
             results.add( new ResultObject(info[0], info[1], info[2]));
         }
         return results;
