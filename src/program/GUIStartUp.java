be0c386ed875779bdc3155a25500f468434a1fd4
JohannesForsting98 <johannes.forsting@gmail.com>
2021-12-02 14:44:37 +0100

    Commit tilføjet hemmelig password skrift

--- src/program/GUIStartUp.java
+++ src/program/GUIStartUp.java
@@ -1,19 +1,13 @@
 package program;
 
-import dolphin.Diciplin;
-import dolphin.Factory;
-import dolphin.Member;
-import fileIO.FilesForeman;
-import foreman.Foreman;
-import foreman.Main;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.time.DateTimeException;
-import java.util.NoSuchElementException;
-    public class GUIStartUp extends JFrame implements ActionListener {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GUIStartUp extends JFrame implements ActionListener, KeyListener {
         JButton button;
         JLabel username;
         JLabel password;
@@ -21,6 +15,7 @@ import java.util.NoSuchElementException;
         JLabel pic;
         JTextField usernameField;
         JPasswordField passwordField;
+
 
         final static String[] usernames = {"Coach", "Cashier", "Foreman"};
         final static String[] passwords = {"Coach123", "Cashier123", "Foreman123"};
@@ -32,6 +27,7 @@ import java.util.NoSuchElementException;
             this.setTitle("Password");
             this.setDefaultCloseOperation(1);
             this.setLayout(new FlowLayout());
+
 
             username = new JLabel("Username");
             this.username.setPreferredSize(new Dimension(100, 40));
@@ -48,6 +44,8 @@ import java.util.NoSuchElementException;
             button = new JButton("Login");
             this.button.setPreferredSize(new Dimension(200, 50));
             this.button.addActionListener(this);
+            this.button.addKeyListener(this);
+
 
             this.pic = new JLabel();
             ImageIcon image = new ImageIcon("src/resources/Dolphin2.jpg");
@@ -67,31 +65,52 @@ import java.util.NoSuchElementException;
             this.setIconImage(icon.getImage());
         }
 
+        @Override
+        public void keyTyped(KeyEvent e) {
+        }
+
+        public void keyPressed(KeyEvent e){
+            if (e.getKeyCode()==KeyEvent.VK_ENTER){
+                logInAttempt();
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
+
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == button){
-                String usernameText = usernameField.getText();
-                String passwordText = passwordField.getText();
-                if (usernameText.equals(usernames[0]) && passwordText.equals(passwords[0])){
-                    logIn = 1;
-                    System.out.println("login succesfull.");
-                    //Runtime.getRuntime().exit(0);
-                    this.dispose();
-                }
-                else if (usernameText.equals(usernames[1]) && passwordText.equals(passwords[1])){
-                    logIn = 2;
-                    System.out.println("login succesfull.");
+                logInAttempt();
+            }
 
-                    this.dispose();
-                }
-                else if (usernameText.equals(usernames[2]) && passwordText.equals(passwords[2])){
-                    logIn = 3;
-                    System.out.println("login succesfull.");
-                    this.dispose();
-                }
-                else{
-                    System.out.println("Username or password is incorrect. Please try again.");
-                }
 
+        }
+
+
+        public void logInAttempt(){
+            String usernameText = usernameField.getText();
+            String passwordText = passwordField.getText();
+            if (usernameText.equals(usernames[0]) && passwordText.equals(passwords[0])){
+                logIn = 1;
+                System.out.println("login succesfull.");
+                //Runtime.getRuntime().exit(0);
+                this.dispose();
+            }
+            else if (usernameText.equals(usernames[1]) && passwordText.equals(passwords[1])){
+                logIn = 2;
+                System.out.println("login succesfull.");
+
+                this.dispose();
+            }
+            else if (usernameText.equals(usernames[2]) && passwordText.equals(passwords[2])){
+                logIn = 3;
+                System.out.println("login succesfull.");
+                this.dispose();
+            }
+            else{
+                System.out.println("Username or password is incorrect. Please try again.");
             }
         }
     }
